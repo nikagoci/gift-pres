c954baf6fe2b670f629f70fef7722ef426e805d4
nikagoci <dato.svanidze2005@mail.ru>
2023-01-19 12:52:08 +0400

    change ui of single product

--- frontend/src/components/SingleProduct.js
+++ frontend/src/components/SingleProduct.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const SingleProduct = ({data}) => {
   return (
-    <Link to={`/product/${data._id}`} className="p-10 w-[80%] bg-gray-200 cursor-pointer mb-10 rounded">
+    <Link to={`/product/${data._id}`} className="p-10 w-[80%] bg-[#343434] text-white cursor-pointer mb-10 rounded">
             <div className="relative block h-48 overflow-hidden rounded">
               <img
                 alt="ecommerce"
@@ -12,10 +12,10 @@ const SingleProduct = ({data}) => {
               />
             </div>
             <div className="mt-4">
-              <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">
+              <h3 className="mb-1 text-xs tracking-widest title-font">
                 {data.gender}
               </h3>
-              <h2 className="text-lg font-medium text-gray-900 title-font">
+              <h2 className="text-lg font-medium text-[#65B2A8] title-font">
                 {data.product}
               </h2>
               <p className="mt-1">{data.condition}</p>
